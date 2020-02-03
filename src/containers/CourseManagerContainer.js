8eb7a8f56d25e01db29723e190204d5fc4092156
Isabel B <isabelbolger@hotmail.com>
2020-02-03 18:08:06 -0500

    course manager container

--- src/containers/CourseManagerContainer.js
+++ src/containers/CourseManagerContainer.js
@@ -1,68 +1,102 @@
 import React from 'react'
-import CourseHeadingComponent from "../components/CourseHeadingComponent";
-import CourseTableComponent from "../components/CourseTableComponent";
+import CourseHeadingComponent from"../components/CourseHeadingComponent";
+import CourseTableComponent from"../components/CourseTableComponent";
 import CourseService from "../services/CourseService";
+import CourseEditor from '../components/CourseEditorComponent';
 
 class CourseManagerContainer extends React.Component {
-    state = {
-        courses: [
-            {_id: '123', title: 'Course A'},
-            {_id: '234', title: 'Course B'},
-            {_id: '345', title: 'Course C'},
-            {_id: '456', title: 'Course D'}
-        ]
-    }
     
     constructor() {
       super();
       this.courseService = new CourseService();
-      // this.state = {
-      //   courses2: this.courseService.findAllCourses()
-      // }
+      this.state = {
+        courses: [],
+        button: true,
+        isEditing: false,
+        showCourseEditor: false,
+      }
+    }
+
+    showCourseEditor = () => {
+        this.setState({
+            showCourseEditor: true,
+        })
+    }
+
+    componentDidMount = () => {
+        this.courseService.findAllCourses()
+        .then(courses => this.setState({
+            courses: courses
+        }))
     }
 
     deleteCourse = (deletedToCourse) => {
-        this.setState(prevState => ({
-                courses: prevState.courses.filter(course =>
-                    course._id !== deletedToCourse._id
-                )
-            })
-        )
-
-        // this.setState({
-        //   courses: this.courseService.deleteCourse(deletedToCourse)
-        // })
+        
+        console.log(deletedToCourse);
+        this.courseService.deleteCourse(deletedToCourse._id).then( () => this.setState({
+                courses: this.state.courses.filter(course => course._id !== deletedToCourse._id)
+        }));
     }
 
-    addCourse = () => {
-        this.setState(prevState => ({
-            courses: [...prevState.courses, {
-                _id: (new Date()).getTime() + '',
-                title: 'New Course'
-            }]
-        })
-        )
-        
-        this.setState({
-          courses: this.courseService.addcourse(null)
-        })
+    addCourse = (newName) => {
+        this.courseService.createCourse({
+            title: newName,
+        }).then(actualCourse => this.setState({
+            courses: [
+                ...this.state.courses,
+                actualCourse,
+            ]
+        }))
 
     }
 
     editCourse = () => {
-      alert('test')
+      this.setState({
+          isEditing: !this.state.isEditing
+      });
+    }
+
+    filterSortButton = () => {
+        this.setState({button: !this.state.button});
+        
+    }
+
+    updateCourse = (courseId, updatedName) => {
+        this.courseService.updateCourse(courseId, {
+            title: updatedName,
+        })
+        .then(()=> {
+            return this.courseService.findAllCourses()
+        })
+        .then(courses => {
+            console.log("updated courses: "+courses)
+            this.setState({
+            courses: courses
+        })}
+        )
+       
     }
 
     render() {
-        return (
-            <div>
-                <CourseHeadingComponent addCourse={this.addCourse}/>
-                <CourseTableComponent
-                    deleteCourse={this.deleteCourse}
-                    courses={this.state.courses}
-                    editCourse={this.editCourse}/>
-            </div>
-        );
+
+        if(this.state.showCourseEditor) {
+            return(<CourseEditor/>);
+        }  else{
+            return (
+                <div>
+                    <CourseHeadingComponent addCourse={this.addCourse} filtered={this.state.button} filterSortButton={this.filterSortButton}/>
+                    <CourseTableComponent
+                        deleteCourse={this.deleteCourse}
+                        courses={this.state.courses}
+                        editCourse={this.editCourse}
+                        button={this.state.button}
+                        isEditing={this.state.isEditing}
+                        showCourseEditor={this.showCourseEditor}
+                        updateCourse={this.updateCourse}/>
+                </div>
+            );
+        }
+        
     }
 }
 
