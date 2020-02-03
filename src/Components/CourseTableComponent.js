21151ecfa3c9a3ab85182d72e9a883efe25b94da
Isabel B <isabelbolger@hotmail.com>
2020-02-03 18:07:03 -0500

    added course table component

--- src/Components/CourseTableComponent.js
+++ src/Components/CourseTableComponent.js
@@ -1,35 +1,44 @@
 import React from 'react'
 import CourseRowComponent from "./CourseRowComponent";
-import GridComponent from "./GridComponent";
+import CourseCard from "./CourseCardComponent";
+import CourseEditor from "./CourseEditorComponent";
 
 class CourseTableComponent extends React.Component {
 
-
   render() {
-
-    if(true) {
+    if(this.props.button) {
       return(
         <div>
                 {
-                  this.props.courses.map(course =>
-                      <CourseRowComponent
+                  this.props.courses.map(course =>{
+                      return (
+                        <CourseRowComponent
                           deleteCourse={this.props.deleteCourse}
                           key={course._id}
+                          id={course._id}
                           course={course}
-                          editCourse={this.props.editCourse}/>
-                  )
+                          editCourse={this.props.editCourse}
+                          showCourseEditor={this.props.showCourseEditor}
+                          updateCourse={this.props.updateCourse}/>
+                      )
+                    })
                 }
         </div>
       )} else  {
+        console.log(this.props.courses);
         return(
-          <div>
+          <div className="row justify-content-around">
                   {
                     this.props.courses.map(course =>
-                        <GridComponent
-                            deleteCourse={this.props.deleteCourse}
-                            key={course._id}
-                            course={course}
-                            editCourse={this.props.editCourse}/>
+                        <CourseCard
+                          deleteCourse={this.props.deleteCourse}
+                          key={course._id}
+                          id={course._id}
+                          course={course}
+                          editCourse={this.props.editCourse}
+                          isEditing={this.props.isEditing}
+                          updateCourse={this.props.updateCourse}
+                          />
                     )
                   }
           </div>
