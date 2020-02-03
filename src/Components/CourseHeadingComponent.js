11f60569275893c213438c560ee46813e7592f32
Isabel B <isabelbolger@hotmail.com>
2020-02-03 18:05:20 -0500

    added course heading component

--- src/Components/CourseHeadingComponent.js
+++ src/Components/CourseHeadingComponent.js
@@ -5,27 +5,44 @@ import {NavDropdown} from 'react-bootstrap';
 import {FormControl} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import {Form, Col, Row} from 'react-bootstrap';
+import $ from 'jquery';
 
 class CourseHeadingComponent extends React.Component {
 
+    state ={
+        newCourseTitle: '',
+    }
+
+    addCourseWithText = () => {
+        this.props.addCourse(this.state.newCourseTitle)
+        
+    }
+
+    updateForm = (newState) => {
+        this.setState(newState)
+    }
 
     render() {
         return (
             <div>
-                <Navbar className="justify-content-between" bg="light" expand="lg">
-                <Navbar.Brand><h1>Course Manager</h1></Navbar.Brand>
-                <Form inline>
-                    <FormControl type="text" placeholder="Course Name" className="mr-sm-2"/>
-                    <Button variant="outline-success" onClick={this.props.addCourse}>+</Button>
-                </Form>
+                <Navbar className="justify-content-between" bg="light">
+                <Navbar.Brand><h1 className="d-none d-md-block">Course Manager</h1></Navbar.Brand>
+                
+                <FormControl
+                onChange={(e) => this.updateForm({
+                    newCourseTitle: e.target.value
+                })} value={this.state.newCourseTitle} id="courseTitle"/>
+                <Button variant="outline-success" onClick={this.addCourseWithText}>+</Button>
+               
                 </Navbar>
+                
                 <Row>
                     <Col>Course Name</Col>
-                    <Col><p>Owned By</p></Col>
-                    <Col><p>Last Modified</p></Col>
+                    <Col className="d-none d-sm-block"><p>Owned By</p></Col>
+                    <Col className="d-none d-md-block"><p>Last Modified</p></Col>
                     <Col sm="2">
-                        <div>
-                            <button>Filter</button>
+                        <div className="justify-content-end">
+                            <button onClick={this.props.filterSortButton}>Filter</button>
                             <button>Sort</button>
                         </div>
                     </Col>
