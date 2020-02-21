994a08d3018079d12bbbb7efe8a45582f781f313
Isabel B <isabelbolger@hotmail.com>
2020-02-20 21:58:12 -0500

    added widget list component

--- src/components/WidgetListComponent.js
+++ src/components/WidgetListComponent.js
@@ -1,89 +1,133 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import HeadingWidget from "./HeadingWidgetComponent";
+import ParagraphWidget from "./ParagraphWidgetComponent";
+import { connect } from "react-redux";
+import widgetService from "../services/WidgetService";
 
 class WidgetList extends React.Component {
-
-  render() {
-    return(
-      <div>
-      <div class="container-fluid">
-      
-      <div class="row d-flex justify-content-around">
-          <button type="button"
-              class="btn btn-success">Save</button>
-          <div class="custom-control custom-switch">
-              <input type="checkbox"
-                  class="custom-control-input"
-                  id="customSwitch1"/> <label
-                  class="custom-control-label"
-                  for="customSwitch1">Preview</label>
-          </div>
-      </div>
-
-      <div class="row" id="wbdv-widget-box">
-
-          <h3 class="col">Heading Widget</h3>
-
-          <button type="button"
-              class="btn btn-warning col-1 mb-3 wbdv-arrow">
-              <span class="fa fa-arrow-up"></span>
-          </button>
-
-          <button type="button"
-              class="btn wbdv-arrow btn-warning col-1 mb-3">
-              <span class="fa fa-arrow-down"></span>
-          </button>
-
-          <div class="input-group col-3">
-              <select class="custom-select">
-                  <option>Choose...</option>
-                  <option value="1" selected>Heading</option>
-                  <option value="2">HTML</option>
-                  <option value="3">Slides</option>
-              </select>
-          </div>
-
-          <button type="button"
-              class="btn btn-danger wbdv-exitWidget mb-3 col-.5">x</button>
-
-
-          <div class="input-group mb-3 col-12">
-              <input placeholder="Heading Text"
-                  type="text" class="form-control"/>
-          </div>
-
-          <div class="input-group mb-3 col">
-              <select class="custom-select">
-                  <option selected>Choose...</option>
-                  <option selected value="1">Heading
-                      1</option>
-                  <option value="2">Heading 2</option>
-                  <option value="3">Heading 3</option>
-              </select>
-          </div>
-
-          <div class="input-group mb-3 col-12">
-              <input type="text"
-                  placeholder="Widget name"
-                  class="form-control"/>
-          </div>
-
-          <div class="col">
-              <h3>Preview</h3>
-              <h1>Heading text</h1>
-          </div>
-      </div>
-
-      <div class="d-flex justify-content-end ">
-          <button type="button"
-              class="btn btn-success col-sm-12 col-md-1">+</button>
-      </div>
-    </div>
-    </div>
-    )
+  componentDidMount() {
+    //this.props.findWidgetsForTopic(this.props.topicId);
+   this.props.findAllWidgets();
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.topicId != this.props.topicId) {
+        this.props.findWidgetsForTopic(this.props.topicId);
+    }
+  }
 
+  addWidget() {
+    this.props.addWidget(this.props.topicId, { title: "test" });
+  }
 
+  render() {
+    return (
+      <div>
+        <div class="container-fluid">
+          <div class="row d-flex justify-content-around">
+            <div class="custom-control custom-switch">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="customSwitch1"
+              />{" "}
+            </div>
+          </div>
+
+          <ul>
+            {this.props.widgets.map(
+              widget => //console.log(widget.id+ 'widgetssss')
+                (widget.type === "HEADING" && (
+                  <HeadingWidget
+                    deleteWidget={this.props.deleteWidget}
+                    topicId={this.props.topicId}
+                    widgetId={widget.id}
+                    updateWidget={this.props.updateWidget}
+                  />
+                )) ||
+                (widget.type === "PARAGRAPH" && (
+                  <ParagraphWidget
+                    deleteWidget={this.props.deleteWidget}
+                    updateWidget={this.props.updateWidget}
+                  />
+                ))
+            )}
+          </ul>
+
+          <div class="d-flex justify-content-end ">
+            <button
+              type="button"
+              class="btn btn-success col-sm-12 col-md-1"
+              onClick={() => this.addWidget()}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default WidgetList;
+const stateToPropertyMapper = state => ({
+  widgets: state.widgets.widgets
+});
+
+const dispatcherToPropertyMapper = dispatch => {
+  return {
+    findAllWidgets: () =>
+      fetch(`http://localhost:8080/widgets`)
+        .then(response => response.json())
+        .then(actualWidgets => {
+          return dispatch({
+            type: "FIND_ALL_WIDGETS",
+            widgets: actualWidgets
+          });
+        }),
+
+    findWidgetsForTopic: (tid) =>
+    fetch(`http://localhost:8080/api/topics/${tid}/widgets`)
+        .then(response => response.json())
+        .then(actualWidgets => dispatch({
+            type: "FIND_WIDGETS_FOR_TOPIC",
+            widgets: actualWidgets
+        })),
+
+    addWidget: (topicId, widget) =>
+      fetch(`http://localhost:8080/api/topics/${topicId}/widgets`, {
+        method: "POST",
+        body: JSON.stringify({
+          title: "New  Widget",
+          _id: new Date().getTime() + "",
+        }),
+        headers: {
+          "content-type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(widget => dispatch({ type: "CREATE_WIDGET", newWidget: widget })),
+
+    updateWidget: (wid, widget) =>
+      fetch(`http://localhost:8080/api/widgets/${wid}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+          "content-type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(status => dispatch({ type: "UPDATE_WIDGET", widget: widget })),
+
+    deleteWidget: wid => {
+      return fetch(`http://localhost:8080/api/widgets/${wid}`, {
+        method: "DELETE"
+      })
+        .then(response => response.json())
+        .then(status => dispatch({ type: "DELETE_WIDGET", widgetId: wid }))}
+  };
+};
+
+export default connect(
+  stateToPropertyMapper,
+  dispatcherToPropertyMapper
+)(WidgetList);
